860116779fcfdfd497beaff6f37fe4456f0bc69d
Dima Voit <dmitriy.voyt@gmail.com>
2022-05-15 21:36:02 +0300

    add exchenge api

--- src/components/exchange/Exchange.jsx
+++ src/components/exchange/Exchange.jsx
@@ -1,25 +1,21 @@
 import React from "react";
 import axios from "axios";
+
 import styles from "./exchange.module.scss";
 
 export class Exchange extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      r030: "",
-      txt: "",
-      rate: "",
-      cc: "",
-    };
-  }
+  state = {
+    exchangeRows: [],
+  };
 
-  componentDidMount() {
+  constructor() {
+    super();
     axios
       .get('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
       .then((response) => {
         console.log(response.data);
-        this.setState(response.data);
+        this.setState({ exchangeRows: response.data });
       })
       .catch((error) => {
         console.error(error);
@@ -37,26 +33,14 @@ export class Exchange extends React.Component {
               <th>buy</th>
               <th>sell</th>
             </tr>
-            <tr>
-              <td>USD</td>
-              <td>28.30</td>
-              <td>28.60</td>
-            </tr>
-            <tr>
-              <td>EUR</td>
-              <td>32.00</td>
-              <td>32.50</td>
-            </tr>
-            <tr>
-              <td>GBP</td>
-              <td>37.50</td>
-              <td>39.50</td>
-            </tr>
-            <tr>
-              <td>PLN</td>
-              <td>6.600</td>
-              <td>7.600</td>
-            </tr>
+            
+            {this.state.exchangeRows.map((exchangeRow) => (
+              <tr key={exchangeRow.r030}>
+                <td>{exchangeRow.cc}</td>
+                <td>{exchangeRow.rate}</td>
+              </tr>
+            ))}
+
           </tbody>
         </table>
       </section>
