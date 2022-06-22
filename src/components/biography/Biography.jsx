cf5869021c08c8fa9ddb15681915212340dd6460
Dima Voit <dmitriy.voyt@gmail.com>
2022-06-22 18:09:16 +0300

    create Select components

--- src/components/biography/Biography.jsx
+++ src/components/biography/Biography.jsx
@@ -4,29 +4,45 @@ import PersonTable from "./PersonTable";
 
 import styles from "./biography.module.scss";
 import PersonForm from "../UI/form/PersonForm";
+import MySelect from "../UI/select/MySelect";
 
 const Biography = () => {
-
   const [persons, setPersons] = useState([
-    {id: 1, yearOfBirth: 1000, firstName: 'Jimmy', lastName: 'Moor', description: 'Lorem ipsum 1'},
-    {id: 2, yearOfBirth: 1002, firstName: 'Jimmy', lastName: 'Moor', description: 'Lorem ipsum 2'},
-    {id: 3, yearOfBirth: 1005, firstName: 'Jimmy', lastName: 'Moor', description: 'Lorem ipsum 3'},
+    { id: 1, personBirth: 1000, personName: 'Gimmy', personLastName: 'Door', personDescription: 'Lorem ipsum 1' },
+    { id: 2, personBirth: 412002, personName: 'Jimmy', personLastName: 'Poor', personDescription: 'Lorem ipsum 2' },
+    { id: 3, personBirth: 91005, personName: 'Jimmy', personLastName: 'Moor', personDescription: 'Lorem ipsum 3' },
   ]);
 
+  const [selectedSort, setSelectedSort] = useState('');
+
   const createPerson = (newPerson) => {
-    setPersons([...persons, newPerson])
-  }
+    setPersons([...persons, newPerson]);
+  };
 
   const removePerson = (person) => {
-    setPersons(person.filter(p => p.id !== person.id))
-  }
+    setPersons(persons.filter((p) => p.id !== person.id));
+  };
+
+  const sortPersons = (sort) => {
+    setSelectedSort(sort);
+    setPersons([...persons].sort((a, b) => a[sort].localeCompare(b[sort])));
+  };
 
   return (
     <div className={styles.biography}>
       <PersonForm create={createPerson} />
-      <PersonTable remove={removePerson} persons={persons} />
+      <MySelect
+        value={selectedSort}
+        onChange={sortPersons}
+        defaultValue="Sort by..."
+        options={[
+          { value: "personBirth", name: "by date" },
+          { value: "personName", name: "by name" }
+        ]}
+      />
+      {persons.length !== 0 ? <PersonTable remove={removePerson} persons={persons} /> : <h4>persons disappeared</h4>}
     </div>
   );
-}
+};
 
 export default Biography;
--- src/components/biography/Person.jsx
+++ src/components/biography/Person.jsx
@@ -6,9 +6,9 @@ import styles from "./biography.module.scss";
 const Person = (props) => {
   return(
     <tr>
-      <td>{props.person.yearOfBirth}</td>
-      <td>{props.person.firstName} {props.person.lastName}</td>
-      <td>{props.person.description}</td>
+      <td>{props.person.personBirth}</td>
+      <td>{props.person.personName} {props.person.personLastName}</td>
+      <td>{props.person.personDescription}</td>
       <td>
         <MyButton className={styles.deleteBtn} onClick={() => props.remove(props.person)}>
           X
